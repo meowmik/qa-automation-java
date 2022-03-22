f185a3da0e8044414dca2c1fe74264d1bce87b48
t.segida <t.segida@tinkoff.ru>
2022-03-22 19:30:21 +0300

    docs: Добавила javadoc в класс ConsolePrinter

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,4 +1,16 @@
+/**
+ * Класс предназначен для вывода информации в консоль
+ * @author Сегида Татьяна
+ *
+ */
 public class ConsolePrinter {
+
+    /**
+     * Метод предназначен для вывода переданной строки в консоль
+     * @param message строка, которую нужно вывести в консолль
+     * @affect Метод ни на что не влияет
+     *
+     */
     public static void print(String message) {
         System.out.println(message);
     }
