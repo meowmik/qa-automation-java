b02dbca3494a3b8102ff3f26d938c65663be0ceb
t.segida <t.segida@tinkoff.ru>
2022-04-05 16:58:20 +0300

    feat: Добавила разделение вывода на страницы и увеличила количество выводимых строк

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -8,7 +8,9 @@ import java.time.Instant;
  * @author Сегида Татьяна
  */
 public class TimestampMessageDecorator {
+
     private static int messageCount = 0;
+    private static final int PAGE_SIZE = 3;
     /**
      * Метод предназначен для обогащенния строки текущей датой, Перед переданной на вход строкой добавляется текущею дату и время
      * @param message строка, к которой нужно добавить текущую дату и время
@@ -16,8 +18,11 @@ public class TimestampMessageDecorator {
      */
     public static String decorate(String message) {
         messageCount++;
-        //noinspection UnnecessaryLocalVariable
-        final var decoratedMessage = String.format("%d %s %s", messageCount, Instant.now(), message);
+        String decoratedMessage;
+        if (messageCount % PAGE_SIZE == 0) {
+            decoratedMessage = String.format("%d %s %s %n" + "---", messageCount, Instant.now(), message);
+        } else
+            decoratedMessage = String.format("%d %s %s", messageCount, Instant.now(), message);
         return decoratedMessage;
     }
 
