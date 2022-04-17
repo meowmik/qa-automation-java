2c965636a9fea939acf3422f18efd5dd5f702ebe
t.segida <t.segida@tinkoff.ru>
2022-04-17 20:01:16 +0300

    feat: Добавила к выводу сообщения уровень значимости.

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,12 +1,24 @@
 package com.tcs.edu;
 
+import com.tcs.edu.decorator.Severity;
 import com.tcs.edu.printer.ConsolePrinter;
-import com.tcs.edu.decorator.TimestampMessageDecorator;
+import com.tcs.edu.decorator.MessageService;
 
 class Application {
     public static void main(String[] args) {
         for (int i = 0; i < 6; i++) {
-            ConsolePrinter.print(TimestampMessageDecorator.decorate("Hello world!"));
+            if (i == 0 || i == 4 ){
+                ConsolePrinter.print(MessageService.decorate("Hello world!", Severity.MINOR));
+            }
+            else
+                if (i == 3 || i == 5){
+                    ConsolePrinter.print(MessageService.decorate("Hello world!", Severity.MAJOR));
+                }
+                else
+                    {
+                        ConsolePrinter.print(MessageService.decorate("Hello world!", Severity.REGULAR));
+                    }
+
         }
     }
 }
---
+++ src/main/java/com/tcs/edu/decorator/MessageService.java
@@ -0,0 +1,34 @@
+package com.tcs.edu.decorator;
+
+import java.time.Instant;
+
+import static com.tcs.edu.decorator.ChoiceSeverity.choiceSeverity;
+
+
+/**
+ * Класс предназначен для обогащения строки.
+ * @author Сегида Татьяна
+ */
+public class MessageService {
+
+    private static int messageCount = 0;
+
+    private static final int PAGE_SIZE = 3;
+
+    /**
+     * Метод предназначен для обогащенния строки порядковым номером, текущей датой и временем, а так же уровнем значимости. Ещё метод добавляет разграничение строк.
+     * @param message строка, которую нужно дополнить
+     * @param level - приоритет, который с помощью метода choiceSeverity конвертируется в уровень значимости
+     * @return обогащённая строка
+     */
+    public static String decorate(String message, Severity level) {
+        messageCount++;
+        String decoratedMessage;
+        if (messageCount % PAGE_SIZE == 0) {
+            decoratedMessage = String.format("%d %s %s %s %n---", messageCount, Instant.now(), message, choiceSeverity(level));
+        } else
+            decoratedMessage = String.format("%d %s %s %s", messageCount, Instant.now(), message, choiceSeverity(level));
+        return decoratedMessage;
+    }
+
+}
