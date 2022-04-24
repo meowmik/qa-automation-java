53055592e111619ac53adca63760226309e4fd6a
t.segida <t.segida@tinkoff.ru>
2022-04-24 19:31:39 +0300

    feat: Добавила новый метод MessageService.print (перегруженный) с возможностью выбора порядка сортировки сообщений. В класс Application добавила две проверки для этого метода

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,11 +1,13 @@
 package com.tcs.edu;
 
 import com.tcs.edu.decorator.MessageDecorator;
+import com.tcs.edu.decorator.MessageOrder;
 import com.tcs.edu.decorator.Severity;
 import com.tcs.edu.printer.ConsolePrinter;
 import com.tcs.edu.service.MessageService;
 
 class Application {
+    //Пример проверки кода без использования api MessageService
     public static void main(String[] args) {
         for (int i = 0; i < 6; i++) {
             if (i == 0 || i == 4 ){
@@ -21,13 +23,17 @@ class Application {
                     }
 
         }
-
+    //Заполнение массива
         String[] array = new String[10] ;
         array[9] = "Hello world!9";
         for (int i=0; i<7;i++){
             array[i] = "Hello world!" + i;
         }
+        //Проверка метода api MessageService.print без параметра сортировкки
         MessageService.print(Severity.REGULAR, array[0], array);
+        //Проверки метода api MessageService.print с параметром сортировки
+        MessageService.print(MessageOrder.ASC, Severity.REGULAR, array[0], array);
+        MessageService.print(MessageOrder.DESC, Severity.REGULAR, array[0], array);
     }
 }
 
--- src/main/java/com/tcs/edu/service/MessageService.java
+++ src/main/java/com/tcs/edu/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.tcs.edu.service;
 
 import com.tcs.edu.decorator.MessageDecorator;
+import com.tcs.edu.decorator.MessageOrder;
 import com.tcs.edu.decorator.Severity;
 import com.tcs.edu.printer.ConsolePrinter;
 
@@ -25,5 +26,28 @@ public class MessageService {
 
         }
     }
-
+    /**
+     * Метод предназначен для вывода сообщений в консоль, порядок вывода сообщений зависит от параметра сортировки.
+     * @param message строка не обогащённая строка
+     * @param level - приоритеты, которые добавятся к строке
+     * @param order - параметр сортировки сообщений
+     */
+    public static void print (MessageOrder order, Severity level,  String message, String... messages){
+        if (order == MessageOrder.ASC){
+            ConsolePrinter.print(MessageDecorator.decorate(message, level));
+            for (String current : messages){
+                if (current != null) {
+                    ConsolePrinter.print(MessageDecorator.decorate(current, level));
+                }
+            }
+        }
+        else{
+            for (int i = messages.length - 1; i >= 0; i--){
+                if (messages[i] != null) {
+                    ConsolePrinter.print(MessageDecorator.decorate(messages[i], level));
+                }
+            }
+            ConsolePrinter.print(MessageDecorator.decorate(message, level));
+        }
+    }
 }
