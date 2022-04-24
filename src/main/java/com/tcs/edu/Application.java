81861447fb17127492521a6f015a555a19c94c89
t.segida <t.segida@tinkoff.ru>
2022-04-24 21:14:38 +0300

    feat: Добавила новый метод MessageService.print (перегруженный) с возможностью выбора порядка сортировки сообщений и возможностью избежания дублирования сообщений

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,5 +1,6 @@
 package com.tcs.edu;
 
+import com.tcs.edu.decorator.Doubling;
 import com.tcs.edu.decorator.MessageDecorator;
 import com.tcs.edu.decorator.MessageOrder;
 import com.tcs.edu.decorator.Severity;
@@ -7,26 +8,24 @@ import com.tcs.edu.printer.ConsolePrinter;
 import com.tcs.edu.service.MessageService;
 
 class Application {
-    //Пример проверки кода без использования api MessageService
     public static void main(String[] args) {
+        //Пример проверки кода без использования api MessageService
         for (int i = 0; i < 6; i++) {
-            if (i == 0 || i == 4 ){
+            if (i == 0 || i == 4) {
                 ConsolePrinter.print(MessageDecorator.decorate("Hello world!", Severity.MINOR));
+            } else if (i == 3 || i == 5) {
+                ConsolePrinter.print(MessageDecorator.decorate("Hello world!", Severity.MAJOR));
+            } else {
+                ConsolePrinter.print(MessageDecorator.decorate("Hello world!", Severity.REGULAR));
             }
-            else
-                if (i == 3 || i == 5){
-                    ConsolePrinter.print(MessageDecorator.decorate("Hello world!", Severity.MAJOR));
-                }
-                else
-                    {
-                        ConsolePrinter.print(MessageDecorator.decorate("Hello world!", Severity.REGULAR));
-                    }
 
         }
-    //Заполнение массива
-        String[] array = new String[10] ;
+        //Заполнение массива
+        String[] array = new String[10];
         array[9] = "Hello world!9";
-        for (int i=0; i<7;i++){
+        array[6] = "Hello world!";
+        array[7] = "Hello world!";
+        for (int i = 0; i < 5; i++) {
             array[i] = "Hello world!" + i;
         }
         //Проверка метода api MessageService.print без параметра сортировкки
@@ -34,6 +33,13 @@ class Application {
         //Проверки метода api MessageService.print с параметром сортировки
         MessageService.print(MessageOrder.ASC, Severity.REGULAR, array[0], array);
         MessageService.print(MessageOrder.DESC, Severity.REGULAR, array[0], array);
+        //Проверки метода api MessageService.print с параметром сортировки и с параметром distinct
+        MessageService.print(Doubling.DISTINCT, MessageOrder.ASC, Severity.REGULAR, array[0], array);
+        MessageService.print(Doubling.DISTINCT, MessageOrder.DESC, Severity.REGULAR, array[0], array);
+        //Проверки метода api MessageService.print с параметром сортировки и с параметром doubles
+        MessageService.print(Doubling.DOUBLES, MessageOrder.ASC, Severity.REGULAR, array[0], array);
+        MessageService.print(Doubling.DOUBLES, MessageOrder.DESC, Severity.REGULAR, array[0], array);
+
     }
 }
 
--- src/main/java/com/tcs/edu/service/MessageService.java
+++ src/main/java/com/tcs/edu/service/MessageService.java
@@ -1,48 +1,54 @@
 package com.tcs.edu.service;
 
+import com.tcs.edu.decorator.Doubling;
 import com.tcs.edu.decorator.MessageDecorator;
 import com.tcs.edu.decorator.MessageOrder;
 import com.tcs.edu.decorator.Severity;
 import com.tcs.edu.printer.ConsolePrinter;
 
+import java.util.Objects;
+
 
 /**
  * Класс предназначен для обогащения строки.
+ *
  * @author Сегида Татьяна
  */
 public class MessageService {
 
     /**
      * Метод предназначен для вывода сообщений в консоль.
+     *
      * @param message строка не обогащённая строка
-     * @param level - приоритеты, которые добавятся к строке
+     * @param level   - приоритеты, которые добавятся к строке
      */
-    public static void print (Severity level, String message, String... messages) {
+    public static void print(Severity level, String message, String... messages) {
         ConsolePrinter.print(MessageDecorator.decorate(message, level));
-        for (String current : messages){
+        for (String current : messages) {
             if (current != null) {
                 ConsolePrinter.print(MessageDecorator.decorate(current, level));
             }
 
         }
     }
+
     /**
      * Метод предназначен для вывода сообщений в консоль, порядок вывода сообщений зависит от параметра сортировки.
+     *
      * @param message строка не обогащённая строка
-     * @param level - приоритеты, которые добавятся к строке
-     * @param order - параметр сортировки сообщений
+     * @param level   - приоритеты, которые добавятся к строке
+     * @param order   - параметр сортировки сообщений
      */
-    public static void print (MessageOrder order, Severity level,  String message, String... messages){
-        if (order == MessageOrder.ASC){
+    public static void print(MessageOrder order, Severity level, String message, String... messages) {
+        if (order == MessageOrder.ASC) {
             ConsolePrinter.print(MessageDecorator.decorate(message, level));
-            for (String current : messages){
+            for (String current : messages) {
                 if (current != null) {
                     ConsolePrinter.print(MessageDecorator.decorate(current, level));
                 }
             }
-        }
-        else{
-            for (int i = messages.length - 1; i >= 0; i--){
+        } else {
+            for (int i = messages.length - 1; i >= 0; i--) {
                 if (messages[i] != null) {
                     ConsolePrinter.print(MessageDecorator.decorate(messages[i], level));
                 }
@@ -50,4 +56,75 @@ public class MessageService {
             ConsolePrinter.print(MessageDecorator.decorate(message, level));
         }
     }
+
+    /**
+     * Метод предназначен для вывода сообщений в консоль, порядок вывода сообщений зависит от параметра сортировки.
+     *
+     * @param message строка не обогащённая строка
+     * @param level   - приоритеты, которые добавятся к строке
+     * @param order   - параметр сортировки сообщений
+     */
+    public static void print(Doubling doubling, MessageOrder order, Severity level, String message, String... messages) {
+        if (doubling == Doubling.DISTINCT) {
+            if (order == MessageOrder.ASC) {
+                String[] array = new String[messages.length + 1];
+                ConsolePrinter.print(MessageDecorator.decorate(message, level));
+                array[0] = message;
+                int n = 1;
+                for (String current : messages) {
+                    if (current != null) {
+                        int j = 0;
+                        boolean flag = false;
+                        while (j < array.length && !flag) {
+                            if (Objects.equals(current, array[j])) {
+                                flag = true;
+                            }
+                            j++;
+                        }
+                        if (!flag) {
+                            ConsolePrinter.print(MessageDecorator.decorate(current, level));
+                            array[n] = current;
+                            n++;
+                        }
+                    }
+                }
+            } else {
+                String[] array = new String[messages.length + 1];
+                int n = 0;
+                for (int i = messages.length - 1; i >= 0; i--) {
+                    if (messages[i] != null) {
+                        int j = 0;
+                        boolean flag = false;
+                        while (j < array.length && !flag) {
+                            if (Objects.equals(messages[i], array[j])) {
+                                flag = true;
+                            }
+                            j++;
+                        }
+                        if (!flag) {
+                            ConsolePrinter.print(MessageDecorator.decorate(messages[i], level));
+                            array[n] = messages[i];
+                            n++;
+                        }
+                    }
+                }
+                int j = 0;
+                boolean flag = false;
+                while (j < array.length && !flag) {
+                    if (Objects.equals(message, array[j])) {
+                        flag = true;
+                    }
+                    j++;
+                }
+                if (!flag) {
+                    ConsolePrinter.print(MessageDecorator.decorate(message, level));
+                }
+
+            }
+        } else {
+            print(order, level, message, messages);
+        }
+    }
+
+
 }
